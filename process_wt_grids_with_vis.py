887212b9b18cd526d34f838084aa8208046dd1e2
Nicoleta Cristea <cristn@uw.edu>
2019-12-02 15:39:47 -0800

    checked x and y grids

--- process_wt_grids_with_vis.py
+++ process_wt_grids_with_vis.py
@@ -8,10 +8,11 @@ Created on Wed May  8 13:21:02 2019
 import numpy as np
 import xarray as xr
 import matplotlib.pyplot as plt
-from scipy.io import loadmat
+#from scipy.io import loadmat
 from xrviz.dashboard import Dashboard
 import pandas as pd
 import seaborn as sns
+import pickle
 #import csv
 
 # <codecell>
@@ -19,21 +20,19 @@ path = '/Users/carina/Desktop/data/Water_table_skagit/Map.Soil.TableDepth.asc.hi
 
 values = np.loadtxt(path)
 # <codecell>
-path_grids = '/Users/carina/Desktop/code/SkagitLandslideHazards/y_x_grids.mat'
+path_grids = '/Users/carina/Desktop/code/SkagitLandslideHazards/grids.pkl'
 
 # <codecell>
-grids = loadmat(path_grids)
-#wd = pd.DataFrame(data=values[1:,1:],    # values
-#             index=data[1:,0],    # 1st column as index
-#             columns=data[0,1:])  # 1st row as the column names 
+#grids = loadmat(path_grids)
+
+with open(path_grids, 'rb') as f:
+    grids = pickle.load(f)
+    
 # <codecell>
 path_dates = '/Users/carina/Desktop/data/Water_table_skagit/dates_max_sat_sauk_old_files /export_historic_dates'
-#dates = np.loadtxt(path_dates + "export_historic_dates",  delimiter='\t')
-#in_txt = csv.reader(open((path_dates + "export_historic_dates"), "rb"), delimiter = '\t')
-#list(csv.reader(open(path_dates+'export_historic_dates.txt', 'rb'), delimiter='\t'))
 
 dates = pd.read_csv(path_dates, sep=" ", header=None)
-#df = pd.read_fwf('path_dates + "export_historic_dates")
+
 dates.columns = ["Map", "txt", "number", "date"] #this works with the new format
 #dates.columns = ["Index","Map", "txt", "number", "date"] #this works with the new format
 dates['date']
@@ -65,45 +64,63 @@ list_arrays = np.vsplit(values, no_time)
 #all_arrays = np.vstack(list_arrays)
 all_arrays =  np.asarray(list_arrays)
 
+
+
 # <codecell>
 #get sizes and reshape arrays to match dimensions and sizes 
 #plot to check results
 
 num = dates['date'].size
+
 years = np.linspace(1, no_time, num)
 
+#get x index for plotting and reshaping 
 x_ = np.linspace(1, 916, num = 916)
 y_ = np.linspace(1, 1020, num = 1020)
 
-#test interpolation
+#test coordinates for interpolation 
+# X_ and Y_ are coordinates for the coarser grid 
 
 x = grids["X_"][:1]
 y = grids["Y_"][:, 1:2]
 
+# <codecell>
 # check grids 
 x = x.T
-#y = y.T
+y = y.reshape(y.shape[0])
+x = x.reshape(x.shape[0])
 
+# <codecell>
+# check grids 
 fig = plt.figure(figsize=(6, 3.2))
 plt.plot(x_, x)
+# <codecell>
 
 fig = plt.figure(figsize=(6, 3.2))
 plt.plot(y_, y)
 
+# <codecell>
+#get x index for plotting and reshaping 
+
 x1_ = np.linspace(1, 916, num = 2725)
 y1_ = np.linspace(1, 916, num = 2720)
+
+#get higher res x1 and y1 
 
 x1 = grids["X_1"][:1]
 y1 =grids["Y_1"][:, 1:2]
 
-# check grids 
+# <codecell>
+# check higher res grids 
 x1 = x1.T
-t = y_.shape
-y = y.reshape(t) 
 
-t = x_.shape
-x = x.reshape(t) 
+# <codecell>
 
+
+y1 = y1.reshape(y1.shape[0])
+x1 = x1.reshape(x1.shape[0])
+
+# <codecell>
 fig = plt.figure(figsize=(6, 3.2))
 plt.plot(x1_, x1)
 
@@ -118,6 +135,8 @@ ds_wt = xr.Dataset(data_vars = {'wt': (('time', 'y', 'x'), all_arrays)})
 
 #%% 
 #ds_wt['time'] = years 
+#assign coordinates
+
 ds_wt['time'] = dates["date"].tolist()
 ds_wt['y'] = y
 ds_wt['x'] = x
@@ -125,10 +144,15 @@ ds_wt['x'] = x
 ds_wt.isel(time=[0]).to_array().plot() #shows upsidedown than imshow
 
 # <codecell>
+#get the numpy for imshow plotting
+a = ds_wt.isel(time=[0]).to_array()
+b = a[0,0].values
+plt.imshow(b)
 
+# <codecell>
 
-y1 = y1.reshape(y1.shape[0])
-x1 = x1.reshape(x1.shape[0])
+# interpolate to get higher res grids
+
 
 dsi = ds_wt.interp(y = y1, x = x1)
 
@@ -138,17 +162,19 @@ dsi = ds_wt.interp(y = y1, x = x1)
 #first image to numpy 
 #one_image_res = dsi.isel(time=[0]).to_array().values
 
-#plot first image 
+#plot first image, select by index
 
-dsi.isel(time=[0]).to_array().plot() #shows upsidedown than imshow
+dsi.isel(time=[0]).to_array().plot() 
 
 # <codecell>
 # or, select by date/name
 dsi.sel(time= '05/31/1969-00').to_array().plot()
  
 # <codecell>
+#save file 
 #dsi.to_netcdf('dtw_historic_with_dates.nc')
-dsi.to_netcdf('dtw_historic_with_dates_netcdf3.nc', format = 'NETCDF3_64BIT')
+
+#dsi.to_netcdf('dtw_historic_with_dates_netcdf3.nc', format = 'NETCDF3_64BIT')
 # <codecell>
 
 dashboard = Dashboard(ds_wt)
@@ -160,7 +186,7 @@ one_location = ds_wt.isel(x=[600], y = [500]).to_array()
 
 #plot one_location 
 
-#ds_wt.isel(x=[500], y = [500]).to_array().plot() #shows upsidedown than imshow
+#ds_wt.isel(x=[500], y = [500]).to_array().plot() 
 
 
 # <codecell> how to define an xrray dataset 
@@ -168,13 +194,5 @@ one_location = ds_wt.isel(x=[600], y = [500]).to_array()
 sns.distplot(one_location);
 
 sns.distplot(one_location, bins=20, kde=False, rug=True); 
-
-
-# ds = xr.Dataset({'temperature': (['x', 'y', 'time'],  temp),
-#   ....:                  'precipitation': (['x', 'y', 'time'], precip)},
-#   ....:                 coords={'lon': (['x', 'y'], lon),
-#   ....:                         'lat': (['x', 'y'], lat),
-#   ....:                         'time': pd.date_range('2014-09-06', periods=3),
-#   ....:                         'reference_time': pd.Timestamp('2014-09-05')})
 
  
